8fc464ce10b37f88cad9ade68454c81f507463d5
chris <sattlub123@gmail.com>
2022-02-03 02:16:54 +0900

    :tada: init the project

---
+++ cmd/server/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	pb "upbit-grpc-provider/protos"
+)
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", 3000))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterTraderServer(grpcServer, &server{})
+	grpcServer.Serve(lis)
+}
+
+type server struct {
+	pb.UnimplementedTraderServer
+}
