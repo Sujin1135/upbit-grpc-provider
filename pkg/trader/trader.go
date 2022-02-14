2d1b6e7ed8365e9db654bf543603e3c8e6cf66fe
chris <sattlub123@gmail.com>
2022-02-14 17:38:31 +0900

    :sparkles: 주문 가능정보 조회 기능

--- pkg/trader/trader.go
+++ pkg/trader/trader.go
@@ -77,3 +77,66 @@ func (t *Trader) GetAccounts() Accounts {
 	fmt.Println(accounts)
 	return accounts
 }
+
+type Bid struct {
+	Currency  string `json:currency`
+	PriceUnit string `json:price_unit`
+	MinTotal  int    `json:min_total`
+}
+
+type Ask struct {
+	Currency  string `json:currency`
+	PriceUnit string `json:price_unit`
+	MinTotal  int    `json:min_total`
+	MaxTotal  int    `json:max_total`
+}
+
+type BidAccount struct {
+	Currency            string `json:currency`
+	Balance             string `json:balance`
+	Locked              string `json:locked`
+	AvgBuyPrice         string `json:avg_buy_price`
+	AvgBuyPriceModified bool   `json:avg_buy_price_modified`
+	UnitCurrency        string `json:unit_currency`
+}
+
+type AskAccount struct {
+	Currency            string `json:currency`
+	Balance             string `json:balance`
+	Locked              string `json:locked`
+	AvgBuyPrice         string `json:avg_buy_price`
+	AvgBuyPriceModified bool   `json:avg_buy_price_modified`
+	UnitCurrency        string `json:unit_currency`
+}
+
+type Market struct {
+	Id         string     `json:id`
+	Name       string     `json:name`
+	OrderTypes string[]   `json:order_types`
+	OrderSides string[]   `json:order_sides`
+	Bid        Bid        `json:bid`
+	BidAccount BidAccount `json:bid_account`
+	AskAccount AskAccount `json:ask_account`
+}
+
+type OrderChance struct {
+	BidFee string `json:bid_fee`
+	AskFee string `json:ask_fee`
+	Market Market `json:market`
+	Ask    Ask    `json:ask`
+	State  string `json:state`
+}
+
+func (t *Trader) Buy() OrderChance {
+	req, err := newHttpRequest(http.MethodGet, "orders/chance")
+	if err != nil {
+		log.Errorf("Occurred an erro when create a new http request to get an info of orders change, %e", err)
+	}
+	res, _ := t.httpClient.Do(req)
+	defer req.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	var orderChance OrderChance
+
+	json.Unmarshal(body, &orderChance)
+	return orderChance
+}
